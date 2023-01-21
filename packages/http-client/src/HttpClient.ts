8044edbc6f7e99096fd49839aeb5100514a4738f
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2023-01-21 21:15:09 +0100

    Add post method

--- packages/http-client/src/HttpClient.ts
+++ packages/http-client/src/HttpClient.ts
@@ -10,4 +10,8 @@ export class HttpClient {
   async get<R>(url: string, params?: Record<string, unknown>) {
     return this.client.get<R>(url, { params });
   }
+
+  async post<R>(url: string, data: unknown) {
+    return this.client.post<R>(url, data);
+  }
 }
